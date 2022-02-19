da21f9046d14316f2414037c8474608bcf16eb09
crnbaker <christian.baker@kcl.ac.uk>
2022-02-19 08:06:40 +0000

    Mixer now keeps track of voices using slice

--- mixer/mixer.go
+++ mixer/mixer.go
@@ -1,18 +1,14 @@
 package mixer
 
 import (
-	"sync"
-
 	"github.com/crnbaker/gostringsynth/errors"
 	"github.com/crnbaker/gostringsynth/sources"
 	"github.com/gordonklaus/portaudio"
 )
 
-var mutex = &sync.RWMutex{}
-
 type Mixer struct {
 	*portaudio.Stream
-	synthFunctions map[int]sources.SynthFunction
+	synthFunctions []sources.SynthFunction
 }
 
 func (m *Mixer) addStream(stream *portaudio.Stream) {
@@ -20,23 +16,11 @@ func (m *Mixer) addStream(stream *portaudio.Stream) {
 }
 
 func (m *Mixer) addSynthFunction(synthFunction sources.SynthFunction) {
-	keyUnavailable := true
-	var newKey int
-	for i := 0; keyUnavailable; i++ {
-		if _, ok := m.synthFunctions[i]; !ok {
-			keyUnavailable = false
-			newKey = i
-		}
-	}
-	mutex.Lock()
-	m.synthFunctions[newKey] = synthFunction
-	mutex.Unlock()
+	m.synthFunctions = append(m.synthFunctions, synthFunction)
 }
 
-func (m *Mixer) deleteSynthFunctionByKey(key int) {
-	mutex.Lock()
-	delete(m.synthFunctions, key)
-	mutex.Unlock()
+func (m *Mixer) deleteSynthFunctionByIndex(i int) {
+	m.synthFunctions = append(m.synthFunctions[:i], m.synthFunctions[i+1:]...)
 }
 
 func (m *Mixer) output(out [][]float32) {
@@ -47,25 +31,26 @@ func (m *Mixer) output(out [][]float32) {
 	}
 	// Add samples values synthesized by currently active voices
 	for i := range out[0] {
-		for key, f := range m.synthFunctions {
+		for j, f := range m.synthFunctions {
 			newSample := f.Synthesize()
 			out[0][i] += newSample
 			out[1][i] += newSample
-			f.AgeInSamples++
-			m.synthFunctions[key] = f // Use key to assign because f is a copy
+			m.synthFunctions[j].AgeInSamples++ // Use index because f is a copy
 		}
 	}
 	// Destroy voices that are past their lifetime
-	for key, f := range m.synthFunctions {
+	numRemoved := 0
+	for i, f := range m.synthFunctions {
 		if f.AgeInSamples > f.LifetimeInSamples {
-			m.deleteSynthFunctionByKey(key)
+			m.deleteSynthFunctionByIndex(i - numRemoved)
+			numRemoved++
 		}
 	}
 }
 
 func newMixer(sampleRate float64) *Mixer {
 
-	synthFunctions := make(map[int]sources.SynthFunction)
+	synthFunctions := make([]sources.SynthFunction, 0)
 
 	mixer := &Mixer{nil, synthFunctions}
 
